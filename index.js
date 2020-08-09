94e450a03e0d54575635cb0b283d744e7dbe1aac
Nick <nick@gmail.com>
2020-08-09 23:06:19 +0600

    11

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.set('view engine', 'hbs')
 app.set('views', 'views')
 
 app.use(express.static(path.join(__dirname, 'public')))
-app.use('/image', express.static(path.join(__dirname, 'image')))
+// app.use('/image', express.static(path.join(__dirname, 'image')))
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
