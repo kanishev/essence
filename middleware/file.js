6650f58eb453f3a48a8ca3a088634fe3b385234e
Nick <nick@gmail.com>
2020-08-09 23:48:24 +0600

    14

--- middleware/file.js
+++ middleware/file.js
@@ -2,7 +2,7 @@ const multer = require('multer')
 
 const storage = multer.diskStorage({
     destination(req, file, cb) {
-        cb(null, 'image')
+        cb(null, '/image')
     },
     filename(req, file, cb) {
         cb(null, file.originalname)
